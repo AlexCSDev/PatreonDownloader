5a08a6e6898fb0bad4e8d01a95efa8fefcb75505
AlexCSDev <942091+AlexCSDev@users.noreply.github.com>
2023-03-31 23:23:08 +0300

    [Hotfix] Disabled datadome cookie check

--- PatreonDownloader.App/Properties/AssemblyInfo.cs
+++ PatreonDownloader.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("24.0.0.0")]
-[assembly: AssemblyFileVersion("24.0.0.0")]
+[assembly: AssemblyVersion("25.0.0.0")]
+[assembly: AssemblyFileVersion("25.0.0.0")]
