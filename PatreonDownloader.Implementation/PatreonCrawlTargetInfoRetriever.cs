e80567c60f8a4bc531ecbf50ff278b8b56589b6f
Skyl3lazer <derekjjjj@gmail.com>
2023-09-19 07:33:47 -0400

    Space in regex causing failure

--- PatreonDownloader.Implementation/PatreonCrawlTargetInfoRetriever.cs
+++ PatreonDownloader.Implementation/PatreonCrawlTargetInfoRetriever.cs
@@ -35,7 +35,7 @@ namespace PatreonDownloader.Implementation
             {
                 string pageHtml = await _webDownloader.DownloadString(url);
 
-                Regex regex = new Regex("\"self\": \"https:\\/\\/www\\.patreon\\.com\\/api\\/campaigns\\/(\\d+)\"");
+                Regex regex = new Regex("\"self\":\"https:\\/\\/www\\.patreon\\.com\\/api\\/campaigns\\/(\\d+)\"");
                 Match match = regex.Match(pageHtml);
                 if (!match.Success)
                 {
