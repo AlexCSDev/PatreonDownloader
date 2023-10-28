d201c1434a62df19502043dbc102ce07be54b8a8
AlexCSDev <942091+AlexCSDev@users.noreply.github.com>
2023-10-28 23:04:29 +0300

    Forgot to update UDP

--- PatreonDownloader.App/Properties/AssemblyInfo.cs
+++ PatreonDownloader.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("26.0.0.0")]
-[assembly: AssemblyFileVersion("26.0.0.0")]
+[assembly: AssemblyVersion("27.0.0.0")]
+[assembly: AssemblyFileVersion("27.0.0.0")]
