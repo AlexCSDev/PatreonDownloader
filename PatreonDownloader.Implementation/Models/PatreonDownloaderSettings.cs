a6b80841ff067c496f73b107957aa062c0923872
AlexCSDev <942091+AlexCSDev@users.noreply.github.com>
2024-09-18 21:50:12 +0300

    Lib update, update to .net 8, copyright update

--- PatreonDownloader.Implementation/Models/PatreonDownloaderSettings.cs
+++ PatreonDownloader.Implementation/Models/PatreonDownloaderSettings.cs
@@ -50,7 +50,7 @@ namespace PatreonDownloader.Implementation.Models
         public bool IsUseLegacyFilenaming { get; init; }
 
         public string LoginPageAddress { get { return "https://www.patreon.com/login"; } }
-        public string LoginCheckAddress { get { return "https://www.patreon.com/api/badges?json-api-version=1.0&include=[]"; } }
+        public string LoginCheckAddress { get { return "https://www.patreon.com/api/badges?json-api-version=1.0&json-api-use-default-includes=false&include=[]"; } }
         public string CaptchaCookieRetrievalAddress { get { return "https://www.patreon.com/home"; } }
         public Uri RemoteBrowserAddress { get; init; }
         public bool IsHeadlessBrowser { get; init; }
